da1db93a9eb21bd8bf58bc32812f1dde4b5bbf86
besiobu <brian.esiobu@gmail.com>
2020-10-20 23:05:37 +0200

    Add helper funtion to format y labels in plots.

---
+++ covid-19/src/visualizations/utils.py
@@ -0,0 +1,34 @@
+def get_vlines(ymin, ymax, k=4):
+    """
+
+    Calculate and format y ticks and y labels.
+
+    """
+
+    def fmt_ytick(y):
+        
+        B = 10 ** 9
+        M = 10 ** 6
+        K = 10 ** 3
+        
+        if y >= B:
+            y, u = y / B, 'B'
+        elif y >= 10 ** 6:
+            y, u = y / M, 'M'            
+        elif y >= 10 ** 3:
+            y, u = y / K, 'K'            
+        elif y <= 10 ** 3:
+            y, u = int(y), ''
+        else: 
+            u = ''
+            
+        # Remove .0 from label
+        label = f'{y:.1f}{u}'
+        label = label.replace('.0', '')
+        return label
+        
+    dy = (ymax - ymin)
+    yticks = [i / k * dy for i in range(0, k + 1)]
+    ylabels = [ fmt_ytick(y) for y in yticks]
+    
+    return yticks, ylabels
